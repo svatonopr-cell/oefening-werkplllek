7da2ea1d7c63507ced4fd9c142abbc66be24da1f
Svatonopr <svatonopr@gmail.com>
2025-11-06 14:08:30 +0100

    DEMO-05

--- Program.cs
+++ Program.cs
@@ -13,19 +13,25 @@ namespace App
         static void Main()
         {
             const string user = "Admin", password = "P@sswoord123";
-
-            Console.Write("Gebruikersnaam: ");
-            string inputUser = Console.ReadLine();
-            Console.Write("Wachtwoord: ");
-            string inputPassword = Console.ReadLine();
-            if (inputUser == user && inputPassword == password)
-            {
-                Console.WriteLine($"Welkom, {inputUser}!");
-                return;
-
-            }
+            int tries = 0;
+        while (tries < 3)
+        {
+        Console.Write("Gebruikersnaam: ");
+        string inputUser = Console.ReadLine();
+        Console.Write("Wachtwoord: ");
+        string inputPassword = Console.ReadLine();
+        if (inputUser == user && inputPassword == password)
+        {
+        Console.WriteLine($"Welkom, {inputUser}!");
+        return;
+        }
+        tries++;
+        Console.WriteLine($"Foutieve login.\n{3-tries} pogingen over.");
+        }
+        Console.WriteLine("Te veel pogingen. Toegang geweigerd.");
+        }
         }
     }
-}
+
     
 
